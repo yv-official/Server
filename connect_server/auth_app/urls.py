2850baaf96e79a88a09107c412f59aca195c229e
Ashish Singh <ashishofficial47@gmail.com>
2020-04-24 23:05:44 +0530

    initial backend server

---
+++ connect_server/auth_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+from rest_framework.authtoken.views import obtain_auth_token
+
+urlpatterns = [
+    path('api/auth/test', views.HelloView.as_view(), name="hello"),
+    path('api/auth/api-token-auth/', obtain_auth_token, name="api_token_auth" ),
+
+]
