34eefc60a3c978e95016b1f8cea29a4b128e9c19
Logan-47 <ashishofficial47@gmail.com>
2020-07-10 01:32:55 +0530

    CORS changes

--- server/__init__.py
+++ server/__init__.py
@@ -15,7 +15,7 @@ app.config['JWT_SECRET_KEY'] = JWT_SECRET_KEY
 app.config['JWT_BLACKLIST_ENABLED'] = True
 app.config['JWT_BLACKLIST_TOKEN_CHECKS'] = ['access', 'refresh']
 
-cors = CORS(app, resources={r"/auth/*": {"origins": config.origins }})
+cors = CORS(app, resources={r"/api/*": {"origins": config.origins }})
 jwt = JWTManager(app)
 db = MongoEngine(app)
 
